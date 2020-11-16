c8609ad33be4a1d022b98092375e9dbd02691546
efsam <E.salimi75@ymail.com>
2020-11-16 11:35:10 +0330

    Commit my base project

---
+++ src/Person1.java
@@ -0,0 +1,30 @@
+public class Person1 {
+    private String name;
+    private char bead;
+
+    public Person1(String n, char b) {
+        setName(n);
+        setBead(b);
+        display();
+    }
+
+    public void display(){
+        System.out.println(getName() + "  : " + getBead());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getBead() {
+        return bead;
+    }
+
+    public void setBead(char bead) {
+        this.bead = bead;
+    }
+}
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
 public class TicTacToe {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter name of the first player :");
+        String name1 = s.next();
+        System.out.println("Please enter [X] or [O] for first player :");
+        char bead1 = s.next().charAt(0);
+        if (bead1 != 'X') {
+            bead1 = 'O';
+        }
+
+        System.out.println("Please enter name of the second player :");
+        String name2 = s.next();
+        char bead2;
+        if (bead1 == 'X') {
+            bead2 = 'O';
+        }
+        else
+            bead2 = 'X';
+
+        Person1 p1 = new Person1(name1, bead1);
+        Person2 p2 = new Person2(name2, bead2);
+
+    }
 }
